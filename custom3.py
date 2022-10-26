2c947534c492e852d4d3bb262820e61eaeec2e9a
jbird2021 <90277731+jbird2021@users.noreply.github.com>
2022-10-26 12:15:15 -0400

    Create custom3.py

---
+++ custom3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+e
+impwefowefrt jsfewon
+import requests
+
+def main():
+  rewef=res.json()
+  print(json.dumps(data, sort_keys=True, indent=2))
+if __name__ == '__main__':
+  main()ef
