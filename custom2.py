b41a90c41917fe871deb46014815830de0f7583b
jbird2021 <90277731+jbird2021@users.noreply.github.com>
2022-10-26 12:14:52 -0400

    Create custom2.py

---
+++ custom2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import json
+import requests
+
+def main():
+  res=requests.get('https://raw.githubusercontent.com/gamuniz/Inventory_Test/master/custom.json')
+  data=res.json()
+  print(json.dumps(data, sort_keys=True, indent=2))
+if __name__ == '__main__':
+  main()
